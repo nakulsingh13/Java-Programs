be54e40ea08d4b0f135786b3b22695ec3c892603
Nakul Singh Gaur <nakulsingh706@gmail.com>
2022-04-12 02:38:04 +0530

    Update

--- Hello.java
+++ Hello.java
@@ -4,6 +4,14 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello Java");
+        //variables
+        
+        String name = "Aman";
+        String neighbour = "Akku";
+        
+        String friend = neighbour;
+        
+        System.out.println(name+' '+neighbour+' '+friend);
 	}
 
 }
--- Prime.java
+++ Prime.java
@@ -1,9 +1,35 @@
 package javaPrograms;
 
+import java.util.Scanner;
+
 public class Prime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Prime number
+		
+		int n,m=0,flag=0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		
+		m=n/2;
+		
+		for(int i=2;i<m;i++)
+		{
+			if(n%i==0) {
+				
+				flag=1;
+				break;
+			}
+		}
+		
+		if(flag==0) {
+			System.out.println("PRIME NUMBER");
+		}
+		else {
+			System.out.println("NOT PRIME");
+		}
 
 	}
 
