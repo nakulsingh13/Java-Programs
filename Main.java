b265bb6adca26e501797c8be8da7a3f0155a86f8
Nakul Singh Gaur <nakulsingh706@gmail.com>
2022-04-12 02:13:08 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+package javaPrograms;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// This is our first java program
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
