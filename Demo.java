b8c1e03e66ba5de4dc91c06afa01bbbdfb265a8e
Nakul Singh Gaur <nakulsingh706@gmail.com>
2022-04-14 22:46:23 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,13 @@
+package javaPrograms;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		String name = "Nakul Singh";
+		 
+		System.out.println(name.substring(0, 5));
+		
+
+	}
+}
