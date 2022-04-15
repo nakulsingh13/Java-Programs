91b13bff9f75c92b432fabe4ce964315e3bdd150
Nakul Singh Gaur <nakulsingh706@gmail.com>
2022-04-16 00:40:00 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,15 @@
+package javaPrograms;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<i+1;j++) {
+		System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
