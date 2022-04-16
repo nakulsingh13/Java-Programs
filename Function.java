f762fd0d25d133d691e8d7d1a0e101fc0b25637d
Nakul Singh Gaur <nakulsingh706@gmail.com>
2022-04-16 11:43:58 +0530

    Update Function.java

--- Function.java
+++ Function.java
@@ -4,17 +4,28 @@ import java.util.Scanner;
 
 public class Function {
 	
-	public static void printMyName(String name) {
-		System.out.println(name);
-		return;
+//	public static void printMyName(String name) {
+//		System.out.println(name);
+//		return;
+//	}
+	
+	public static int calculateSum(int a, int b) {
+		
+		int sum = a + b;
+		return sum;
+		
+		
 	}
 
 	public static void main(String[] args) {
 		try (// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in)) {
-			String name = sc.next();
+			int a = sc.nextInt();
+			int b = sc.nextInt();
 			
-			printMyName(name);
+			
+			int sum = calculateSum(a,b);
+			System.out.println(sum);
 		}
 	}
 
