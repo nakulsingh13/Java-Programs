0404cacfc70851a126ab67458fbb4c427763b405
Nakul Singh Gaur <nakulsingh706@gmail.com>
2022-04-19 23:43:08 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,9 +1,18 @@
 package javaPrograms;
 
+
+
 public class Arrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] marks = new int[3];
+		marks[0] = 97; // phy
+		marks[1] = 98; // chem
+		marks[2] = 95; // eng
+		
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		System.out.println(marks[2]);
 
 	}
 
