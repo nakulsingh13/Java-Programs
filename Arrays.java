25a4e009d8ff6f618ffa6b36bd46a3dc1d3c838c
Nakul Singh Gaur <nakulsingh706@gmail.com>
2022-04-29 02:45:31 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,22 +1,22 @@
 package javaPrograms;
 
+import java.util.*;
+
 
 
 public class Arrays {
 
 	public static void main(String[] args) {
-int[] marks= {97,98,95};
-		//		int[] marks = new int[3];
-        //		marks[0] = 97; // phy
-        //		marks[1] = 98; // chem
-        //		marks[2] = 95; // eng
-        //		
-		//     System.out.println(marks[0]);
-		//     System.out.println(marks[1]);
-		//     System.out.println(marks[2]);
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+        int numbers[] = new int[size]; 
+        
+        for(int i=0;i<size;i++) {
+			numbers[i] = sc.nextInt();
+		}
 		
-		for(int i=0;i<3;i++) {
-			System.out.println(marks[i]);
+		for(int i=0;i<size;i++) {
+			System.out.println(numbers[i]);
 		}
 
 	}
