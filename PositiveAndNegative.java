f961c1030bb0d4f600b913a96a29a53f9fa4a292
Nakul Singh Gaur <nakulsingh706@gmail.com>
2022-08-26 00:13:07 +0530

    Update PositiveAndNegative.java

--- PositiveAndNegative.java
+++ PositiveAndNegative.java
@@ -5,6 +5,7 @@ public class PositiveAndNegative {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		
 		int num = sc.nextInt();
 		
 		
